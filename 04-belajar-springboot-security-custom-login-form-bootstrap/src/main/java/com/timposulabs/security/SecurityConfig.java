d6d77d5c924b378d8dd42960b8bfc27cf7ee2fcb
Ucup Topekox <ucup.topekox@gmail.com>
2025-12-20 19:41:15 +0800

    init custom login bootstrap

---
+++ 04-belajar-springboot-security-custom-login-form-bootstrap/src/main/java/com/timposulabs/security/SecurityConfig.java
@@ -0,0 +1,55 @@
+package com.timposulabs.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+	// membuat user yang hanya running di memory
+	@Bean
+	public InMemoryUserDetailsManager userDetailsManager() {
+		
+		UserDetails aco = User.builder()
+				.username("aco")
+				.password("{noop}test123")
+				.roles("GUEST")
+				.build();
+		
+		UserDetails ade = User.builder()
+				.username("ade")
+				.password("{noop}test123")
+				.roles("GUEST", "USERS")
+				.build();
+		
+		UserDetails ucup = User.builder()
+				.username("ucup")
+				.password("{noop}test123")
+				.roles("GUEST", "USERS", "ADMIN")
+				.build();				
+		
+		return new InMemoryUserDetailsManager(aco, ade, ucup);
+	}
+	
+	// konfig custom login page
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+		
+		httpSecurity.authorizeHttpRequests(configurer -> 
+					configurer
+						.anyRequest().authenticated()
+				).formLogin(form ->
+					form
+						.loginPage("/loginPage")
+						.loginProcessingUrl("/authentication")
+						.permitAll()
+				);
+		
+		return httpSecurity.build();
+	}
+}
