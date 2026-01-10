f5f988f95735c38f29b69563f18b914615fe90b8
Ucup Topekox <ucup.topekox@gmail.com>
2026-01-10 23:21:09 +0800

    add bcrypt password encoder

---
+++ 11-belajar-springboot-security-jdbc-authentication-bcrypt-password-encoder/query.sql
@@ -0,0 +1,51 @@
+USE `belajar_spring`;
+
+DROP TABLE IF EXISTS `authorities`;
+DROP TABLE IF EXISTS `users`;
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `username` varchar(50) NOT NULL,
+  `password` char(68) NOT NULL,
+  `enabled` tinyint NOT NULL,
+  PRIMARY KEY (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Inserting data for table `users`
+--
+
+INSERT INTO `users` 
+VALUES 
+-- Password = rahasia123 --
+('aco','$2a$12$v8mHo6OSrm/ReDDydgFUneA2n1YIu.j8SFwzFvCjTGuDcTtoewB5W',1), 
+('ade','$2a$12$v8mHo6OSrm/ReDDydgFUneA2n1YIu.j8SFwzFvCjTGuDcTtoewB5W',1),
+('ucup','$2a$12$v8mHo6OSrm/ReDDydgFUneA2n1YIu.j8SFwzFvCjTGuDcTtoewB5W',1);
+
+
+--
+-- Table structure for table `authorities`
+--
+
+CREATE TABLE `authorities` (
+  `username` varchar(50) NOT NULL,
+  `authority` varchar(50) NOT NULL,
+  UNIQUE KEY `authorities_idx_1` (`username`,`authority`),
+  CONSTRAINT `authorities_ibfk_1` FOREIGN KEY (`username`) REFERENCES `users` (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Inserting data for table `authorities`
+--
+
+INSERT INTO `authorities` 
+VALUES 
+('aco','ROLE_GUEST'),
+('ade','ROLE_GUEST'),
+('ade','ROLE_USER'),
+('ucup','ROLE_GUEST'),
+('ucup','ROLE_USER'),
+('ucup','ROLE_ADMIN');
---
+++ 11-belajar-springboot-security-jdbc-authentication-bcrypt-password-encoder/src/main/java/com/timposulabs/security/SecurityConfig.java
@@ -0,0 +1,53 @@
+package com.timposulabs.security;
+
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+	// membuat user konfigurasi dari database
+	@Bean
+	public UserDetailsManager userDetailsManager(DataSource dataSource) {
+		return new JdbcUserDetailsManager(dataSource);		
+	}
+	
+	// Bean untuk Password Encoder BCrypt
+	@Bean
+    public PasswordEncoder passwordEncoder() {
+        // Angka 12 adalah 'High security' (log rounds) cocok production. Standar adalah 10-12.
+        return new BCryptPasswordEncoder(12);
+    }
+	
+	// konfig custom login page
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+		
+		httpSecurity.authorizeHttpRequests(configurer -> 
+					configurer
+						.requestMatchers("/").hasRole("GUEST")
+						.requestMatchers("/user/**").hasAnyRole("USERS", "ADMIN")
+						.requestMatchers("/admin/**").hasRole("ADMIN")
+						.anyRequest().authenticated()
+				).formLogin(form ->
+					form
+						.loginPage("/loginPage")
+						.loginProcessingUrl("/authentication")
+						.permitAll()
+				).logout(logout -> 
+					logout.permitAll()
+				).exceptionHandling(configurer ->
+					configurer.accessDeniedPage("/access-denied")
+				);
+		
+		return httpSecurity.build();
+	}
+}
